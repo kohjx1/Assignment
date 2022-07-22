1ccb9f9854ae49bdb25658893612eb5aa853619d
Glenn Koh <kohjiaglenn@hotmail.com>
2022-07-22 21:23:35 +0800

    2nd commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css"
 
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 
 import Login from "./components/login"
 import Signup from "./components/signup"
@@ -8,6 +8,10 @@ import Signup from "./components/signup"
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to="/">Log In</Link>
+        <Link to="/Signup">Sign up</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/Signup" element={<Signup />} />
--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,11 +1,14 @@
 import React from "react"
-import { Grid, Paper, Avatar, TextField, Button, Typography } from "@mui/material"
+import { Grid, Paper, Avatar, TextField, Button } from "@mui/material"
 import KeyIcon from "@mui/icons-material/Key"
+import { useNavigate } from "react-router-dom"
 
 function login() {
   const paperStyle = { padding: 20, height: "80vh", width: 280, margin: "20px auto" }
   const avatarStyle = { background: "green" }
   const inputStyle = { height: 80 }
+
+  let navigate = useNavigate()
 
   return (
     <Grid>
@@ -20,6 +23,17 @@ function login() {
         <TextField label="Password" placeholder="Enter Password" style={inputStyle} type="password" fullWidth required />
         <Button type="submit" color="primary" variant="contained" fullWidth>
           Sign In
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/Signup")
+          }}
+          type="submit"
+          color="primary"
+          variant="contained"
+          fullWidth
+        >
+          Sign Up
         </Button>
       </Paper>
     </Grid>
