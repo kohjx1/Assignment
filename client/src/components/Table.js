f7bd1aaed8daa7edb30c6b63c03bd63a1ed97753
Glenn Koh <kohjiaglenn@hotmail.com>
2022-07-27 00:35:08 +0800

    Continue working on data grid table

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useContext } from "react"
 import DispatchContext from "../DispatchContext"
 import StateContext from "../StateContext"
 import Axios from "axios"
+import Box from "@mui/material/Box"
+import { DataGrid } from "@mui/x-data-grid"
 
 function Table() {
   // const appDispatch = useContext(DispatchContext)
@@ -19,29 +21,49 @@ function Table() {
       } else {
         setData(response.data)
         // console.log(response)
-        console.log(response.data.length)
+        // console.log(response.data[0])
       }
     } catch (e) {
       console.log("There was an issue with retrieval")
     }
   }
 
-  let USERS = []
-  for (let i = 0; i < data.length; i++) {
-    USERS[i] = {
-      id: data[0].id,
-      username: data[0].username,
-      password: data[0].password,
-      email: data[0].email,
-      status: data[0].status
-    }
-  }
-  console.log(USERS)
+  // let USERS = []
+  // for (let i = 0; i < data.length; i++) {
+  //   USERS[i] = {
+  //     id: data[0].id,
+  //     username: data[0].username,
+  //     password: data[0].password,
+  //     email: data[0].email,
+  //     status: data[0].status
+  //   }
+  // }
+  // console.log(USERS)
+
+  const rows = data
+  console.log(rows)
+
+  const columns = [
+    { field: "id", headerName: "ID", type: "number", width: 90 },
+    { field: "username", headerName: "Username", width: 150 },
+    {
+      field: "email",
+      headerName: "Email",
+      width: 150
+    },
+    { field: "password", headerName: "Password", width: 150 },
+    { field: "status", headerName: "Status", type: "number", width: 150 }
+  ]
 
   return (
-    <button name="button" onClick={getData}>
-      Click me
-    </button>
+    <div>
+      <button name="button" onClick={getData}>
+        Click me
+      </button>
+      <Box sx={{ height: 400, width: "100%" }}>
+        <DataGrid columns={columns} rows={rows} />
+      </Box>
+    </div>
   )
 }
 
